8475a114bbbd81f9680d7c706b22ee975531246d
Rafael Gonzalez <admin@mbxarts.com>
2025-07-07 18:02:00 -0500

    chore: setup Hardhat + TS deps + offline solc compile

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+﻿import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.23",
+        compilerPath: require.resolve("solc/soljson.js"),
+        settings: {},
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    artifacts: "./artifacts",
+    cache: "./cache",
+  },
+};
+
+export default config;
